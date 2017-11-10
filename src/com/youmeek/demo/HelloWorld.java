fe993e8e361d80d016c62c6c0ef7bf0d25aeecd4
windofred <1342670786@qq.com>
2017-11-10 23:01:12 +0800

    IDEA整合GitHub测试

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -6,5 +6,7 @@ package com.youmeek.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("hello china");
     }
 }
